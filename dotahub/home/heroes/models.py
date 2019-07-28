d5d61e2198f8381795afe22e15c0b892099be705
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2019-07-28 20:26:10 +0430

    d

--- dotahub/home/heroes/models.py
+++ dotahub/home/heroes/models.py
@@ -55,7 +55,6 @@ class HeroPropertyImage(models.Model):
 
 class HeroCategory(models.Model):
     name = models.CharField(max_length=60, null=True, blank=False)
-    farsi_name = models.CharField(max_length=60, null=True, blank=False)
 
     panels = [
         FieldPanel('name'), FieldPanel('farsi_name')
