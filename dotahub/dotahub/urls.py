ee62ac0ccc65e92d8c1491084ea1de06b43f5482
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2019-10-04 15:24:41 +0330

    d

--- dotahub/dotahub/urls.py
+++ dotahub/dotahub/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
@@ -17,6 +18,7 @@ urlpatterns = [
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^api/v2/', api_router.urls),
+    url(r'^sitemap\.xml$', sitemap),
 
 ]
 
